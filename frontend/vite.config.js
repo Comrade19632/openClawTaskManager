272b2e615b73eab27379c1969c6912386a8868f8
root <root@vm161818.xxvps.net>
2026-02-09 14:11:16 +0000

    feat: telegram login widget auth + allow domain in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   base: "/taskManager/",
+  server: {
+    allowedHosts: ["vm161818.xxvps.net"],
+  },
+  preview: {
+    allowedHosts: ["vm161818.xxvps.net"],
+  },
 })
